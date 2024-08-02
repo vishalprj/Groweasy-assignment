b55292cb777f821293008c0f5b1286454341ce7a
Vishal Prajapati <85871658+vishalprj@users.noreply.github.com>
2024-08-02 20:08:05 +0530

    🚀 design updated

--- src/app/views/home/index.tsx
+++ src/app/views/home/index.tsx
@@ -4,19 +4,21 @@ import AdBanner from "@/components/Banner";
 const HomePage = () => {
   const { data } = useGetAdBanner();
   return (
-    <div className="ad-banners p-20">
+    <div className="ad-banners pt-3">
+      <h1 className="text-3xl font-bold text-center	">GrowEasy Assignment</h1>
       {data?.map((banner) => (
-        <AdBanner
-          key={banner.id}
-          id={banner.id}
-          title={banner.title}
-          description={banner.description}
-          cta={banner.cta}
-          image={banner.image}
-          background={banner.background}
-          isEdit={true}
-          isStyle={false}
-        />
+        <div key={banner.id} className="ad-banners p-10">
+          <AdBanner
+            id={banner.id}
+            title={banner.title}
+            description={banner.description}
+            cta={banner.cta}
+            image={banner.image}
+            background={banner.background}
+            isEdit={true}
+            isStyle={false}
+          />
+        </div>
       ))}
     </div>
   );
